7ba5873fa2772f416ecd4013d27bc362a0c8f97b
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-06-07 07:42:04 +0000

    [add]ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,51 @@ Rails.application.routes.draw do
 
   # 顧客用
   # URL /customers/sign_in ...
+  scope module: :public do
+    # customers
+    get "customers/unsubscribe" => "customers#unsubscribe"
+    patch "customers/withdraw" => "customers#withdraw"
+    resource :customers, only: [:edit, :update, :index, :show]
+
+    # homes
+    root to: 'homes#top'
+    get '/about' => 'homes#about', as: 'about'
+
+    # camp_items
+    resources :camp_items
+
+    # camps
+    resources :camps
+
+  end
+
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+  
+  
+  
   
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :admin do
+    # homes
+    get '/' => 'homes#top'
+
+    # cunstomers
+    resources :customers
+
+    # camp_items
+    resources :camp_items
+
+    # camps
+    resources :camps
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
