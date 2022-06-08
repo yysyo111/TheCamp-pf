977c85fdfb69f06dacf3e732da4923f530a48ec2
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-06-08 09:00:32 +0000

    [add]プロフィール画像追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,5 +5,14 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_one_attached :profile_image
+  
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
+
 
