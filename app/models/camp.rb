a0aee35c8e2b7cb3fefa8e1dd7a2ca0c011f745a
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-06-11 06:30:57 +0000

    [Add]キャンプ場更新・enum導入・api再設定

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -3,8 +3,10 @@ class Camp < ApplicationRecord
   belongs_to :admin
   has_many :camp_comments, dependent: :destroy
   has_many :camp_favorites, dependent: :destroy
-  
+
   geocoded_by :address
-  after_validation :geocode, if: :address_changed? 
-  
+  after_validation :geocode, if: :address_changed?
+
+  # enum定義(カリキュラム参考)
+  enum area: { hokkaido: 0, tohoku: 1, kanto: 2, chubu: 3, kinki: 4, chugoku: 5, shikoku: 6, kyushu: 7 }
 end
