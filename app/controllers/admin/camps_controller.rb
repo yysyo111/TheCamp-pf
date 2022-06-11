e4c015456108daf2176dbd7bab2149ebcba68bf7
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-06-11 08:41:00 +0000

    [Add]CRUD機能追加

--- app/controllers/admin/camps_controller.rb
+++ app/controllers/admin/camps_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CampsController < ApplicationController
-  
+
   def index
     @camps = Camp.all
   end
@@ -14,23 +14,33 @@ class Admin::CampsController < ApplicationController
 
   def create
     @camp = Camp.new(camp_params)
-    @camp.admin_id = current_admin.id
+    @camp.admin_id = current_admin.id #ログイン中のユーザーの情報を取得
 
     @camp.save
-    redirect_to admin_path
+    redirect_to admin_camps_path
   end
 
   def edit
+    @camp = Camp.find(params[:id])
   end
 
   def update
+    @camp = Camp.find(params[:id])
+    if @camp.update(camp_params)
+      redirect_to admin_camp_path(@camp.id)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @camp = Camp.find(params[:id])
+    @camp.destroy
+    redirect_to admin_camps_path(@camp)
   end
 
   private
   def camp_params
-    params.require(:camp).permit(:admin_id, :name, :address, :phone_number, :area, :latitude, :longitude, :parking)
+    params.require(:camp).permit(:admin_id, :name, :address, :phone_number, :area, :latitude, :longitude, :parking, :camp_image)
   end
 end
--- app/models/camp.rb
+++ app/models/camp.rb
@@ -9,4 +9,15 @@ class Camp < ApplicationRecord
 
   # enum定義(カリキュラム参考)
   enum area: { hokkaido: 0, tohoku: 1, kanto: 2, chubu: 3, kinki: 4, chugoku: 5, shikoku: 6, kyushu: 7 }
+
+  # キャンプ場の画像追加
+  has_one_attached :camp_image
+
+  def get_camp_image(width, height)
+    unless camp_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      camp_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    camp_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
