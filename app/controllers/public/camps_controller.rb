36951cedea6d8d85d23bd16d5410c3e65a007b65
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-06-14 13:08:00 +0000

    [Add]星評価平均実装中

--- app/controllers/public/camps_controller.rb
+++ app/controllers/public/camps_controller.rb
@@ -9,6 +9,7 @@ class Public::CampsController < ApplicationController
     @camp = Camp.find(params[:id])
     @camp_comment = CampComment.new
     @camp_comments = CampComment.where(camp_id: @camp.id)
+    # @camp_avg = @camp.get_camp_review_avg
   end
 
   private
--- app/models/camp.rb
+++ app/models/camp.rb
@@ -24,4 +24,33 @@ class Camp < ApplicationRecord
     end
     camp_image.variant(resize_to_limit: [width, height]).processed
   end
+  
+  def get_camp_review_avg
+    avg_rate = self.camp_comments.group("camp_comments.camp_id").average("camp_comments.rate").values.first
+    if avg_rate > 4.5
+      5
+    elsif 4.0< avg_rate and avg_rate <= 4.5
+      4.5
+    elsif 3.5< avg_rate and avg_rate  <=4.0
+      4.0
+    elsif 3.0< avg_rate and avg_rate <=3.5
+      3.5
+    elsif 2.5< avg_rate and avg_rate <=3.0
+      3.0
+    elsif 2.0< avg_rate and avg_rate <=2.5
+      2.5
+    elsif 1.5< avg_rate and avg_rate <=2.0
+      2.0
+    elsif 1.0< avg_rate and avg_rate <=1.5
+      1.5
+    elsif 0.5 < avg_rate and avg_rate <=1.0
+      1.0
+    elsif 0< avg_rate and avg_rate <=0.5
+      0.5
+    else
+      0
+    end
+  end
 end
+
+
