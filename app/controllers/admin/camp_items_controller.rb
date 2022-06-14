0e07aefdc6467b764635de42d383e75b4c36477a
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-06-14 02:30:32 +0000

    [Add]管理者アイテムレビュー管理画面追加

--- app/controllers/admin/camp_items_controller.rb
+++ app/controllers/admin/camp_items_controller.rb
@@ -1,9 +1,12 @@
 class Admin::CampItemsController < ApplicationController
   before_action :authenticate_admin!
-  
-  def index 
-  end 
-  
-  def show 
-  end 
+
+  def index
+    @camp_items = CampItem.all
+  end
+
+  def show
+    @camp_item = CampItem.find(params[:id])
+    @camp_item_comment = CampItemComment.new
+  end
 end
