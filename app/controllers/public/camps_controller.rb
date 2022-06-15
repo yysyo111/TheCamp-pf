a86fe8cb5beb36516b786a0d2f0dd8742e4b036e
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-06-15 14:20:37 +0000

    []

--- app/controllers/public/camps_controller.rb
+++ app/controllers/public/camps_controller.rb
@@ -2,13 +2,20 @@ class Public::CampsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @camps = Camp.all
+    # 絞り込み表示
+    if params[:area].present?
+      @camps = Camp.where(area: params[:area])
+    else
+      @camps = Camp.all
+    end
+      @value = params[:area]
   end
 
   def show
     @camp = Camp.find(params[:id])
     @camp_comment = CampComment.new
     @camp_comments = CampComment.where(camp_id: @camp.id)
+    # @camp_avg = @camp.get_camp_review_avg
   end
 
   private
