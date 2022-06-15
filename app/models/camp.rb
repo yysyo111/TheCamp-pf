705f5aa22cff8e923ee302fd483183878ee2eb8c
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-06-15 14:31:23 +0000

    [add]修正

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -28,7 +28,8 @@ class Camp < ApplicationRecord
   def get_camp_review_avg
     avg_rate = self.camp_comments.group("camp_comments.camp_id").average("camp_comments.rate").values.first
     avg_rate.nil? ? -1 : (avg_rate * 2).round / 2.0
-    # 平均が空だった場合は(true)-1を返す。また、投稿したけど評価が全て０だった場合は表示する。0だった場合とnilだった場合で分ける。(-1で分ける),三項演算子で分岐(true,false)
+    # 平均が空だった場合は(true)-1を返す。
+    # また、投稿したけど評価が全て０だった場合は表示する。0だった場合とnilだった場合で分ける。(-1で分ける),三項演算子で分岐(true,false)
     #  falseの場合:avg_rateで合計の平均で出ている。その平均を、(avg_rate * 2).round / 2.0←ここで２倍にして四捨五入して、半分にした値がviewで出る
   end
 end
