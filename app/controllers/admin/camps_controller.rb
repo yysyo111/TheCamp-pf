562085ce382ece8767bca8ee6523f00a97e13729
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-06-25 11:35:17 +0000

    [add]管理者口コミ評価順jissou

--- app/controllers/admin/camps_controller.rb
+++ app/controllers/admin/camps_controller.rb
@@ -9,6 +9,16 @@ class Admin::CampsController < ApplicationController
     # 絞り込み表示
     if params[:area].present?
       @camps = Camp.where(area: params[:area]).page(params[:page]).per(3)
+    # 評価の高い順(gem 'order_as_specified'というgem使用している)
+    elsif params[:rate_count]
+      avg_camp_comments = CampComment.group(:camp_id).average(:rate)
+      avg_order_camp_ids = avg_camp_comments.sort {|a,b| b[1]<=>a[1]}.to_h.keys
+      @camps = Camp.where(id: avg_order_camp_ids).order_as_specified(id: avg_order_camp_ids).page(params[:page]).per(3)
+    # 評価の低い順(gem 'order_as_specified'というgem使用している)
+    elsif params[:rate_asc_count]
+      avg_camp_comments = CampComment.group(:camp_id).average(:rate)
+      avg_order_camp_ids = avg_camp_comments.sort {|a,b| a[1]<=>b[1]}.to_h.keys
+      @camps = Camp.where(id: avg_order_camp_ids).order_as_specified(id: avg_order_camp_ids).page(params[:page]).per(3)
     else
       @camps = Camp.all.page(params[:page]).per(3)
     end
