ab960d1104f6e2b4699743155894c71ab0f9c95c
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-06-25 05:04:44 +0000

    [add]退会済みのフラッシュメッセージ表示

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  # before_action :reject_inactive_customer, only: [:create]
+  before_action :reject_inactive_customer, only: [:create]
 
 
   # GET /resource/sign_in
@@ -32,7 +32,7 @@ class Public::SessionsController < Devise::SessionsController
   def reject_inactive_customer
     @customer = Customer.find_by(email: params[:customer][:email])
     if @customer
-      if @customer.valid_password?(params[:customer][:password]) && !@customer.is_deleted
+      if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted
         flash[:notice] = "退会済みのユーザーです。再度ご登録してご利用ください。"
         redirect_to new_customer_registration_path
       end
