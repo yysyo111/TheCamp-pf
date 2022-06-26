7b2ed8e4e34363106fcd6bc96435616ee82a5490
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-06-26 08:37:26 +0000

    [add]

--- app/lib/batch/data_reset.rb
+++
@@ -1,8 +0,0 @@
-# バッチ処理
-# class Batch::DataReset
-#   def self.data_reset
-#     Customer.guest.camp_items.delete_all
-#     p "ゲストユーザーの投稿を全て削除しました"
-#   end
-# end
-
--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,5 @@ module TheCamp
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
 
-    # config.paths.add 'lib', eager_load: true
   end
 end
