27b0d2c83303fdf051813164c7dd0efd205cad84
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-03 07:46:02 +0000

    [add]Cloud Vision API導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_062413) do
+ActiveRecord::Schema.define(version: 2022_07_03_060331) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -148,6 +148,13 @@ ActiveRecord::Schema.define(version: 2022_06_07_062413) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "vision_tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "camp_item_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
