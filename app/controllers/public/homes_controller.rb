f73a3ee18e2fa902dae382a60cce3dc78e8df53a
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-07-15 13:28:00 +0000

    [add]Topランキング表示

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,10 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @camps = Camp.all.order(created_at: :desc).limit(3)
+    # @camps = Camp.all.order(created_at: :desc).limit(3)
+    # 人気キャンプ場の上位３件
+    avg_camp_comments = CampComment.group(:camp_id).average(:rate)
+    avg_order_camp_ids = avg_camp_comments.sort {|a,b| b[1]<=>a[1]}.to_h.keys
+    @camps = Camp.where(id: avg_order_camp_ids).order_as_specified(id: avg_order_camp_ids).limit(3)
   end
 end
